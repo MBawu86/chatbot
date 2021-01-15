9f536a22242fcb1a77ba0d901ab6d156f7e3c9ce
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-15 15:08:47 -0600

    makes first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+print(data["intents"])
+
+words = []
+labels = []
+docs = []
+
+for intent in data["intents"]:
+    for pattern in intent [patterns]:
+    
